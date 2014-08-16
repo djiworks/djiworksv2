cf2e2fac1afb99c76f9d0c913556fca7d98b155a
Djothi Grondin <djothi.grondin@live.fr>
2014-08-16 11:18:35 +0400

    tests

--- index.php
+++ index.php
@@ -1,32 +1,119 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
+	<title> 
+		DjiWorks, Site personnel de Djothi Grondin, Portefolio des photos, du multim&eacute;dia et des d&eacute;veloppements web, mobiles et logiciels
+	</title>
+	
 	<!-- Les meta -->
-    <title> DjiWorks, Site personnel de Djothi Grondin, Portefolio des photographies, du multim&eacute;dia et des d&eacute;veloppements web, mobiles et logiciels</title>
     <meta charset="UTF-8">
-    <meta name="keywords" content="djothi, grondin, site personnel, djothi grondin, photo de djothi, site internet de djothi, site web de djothi, la r&eacute;union, &eacute;le de la r&eacute;union, 974, djiworks"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     
-    <meta name="description" content="DjiWorks est le site personnel de Djothi Grondin vous permettant de d&eacute;couvrir l\'ensemble de ses cr&eacute;ations informatiques, multim&eacute;dias mais aussi photographiques."/>
+    <meta name="keywords" content="djothi, grondin, site personnel, djothi grondin, 
+    photo de djothi, site internet de djothi, site web de djothi, 
+    la r&eacute;union, &eacute;le de la r&eacute;union, 974, djiworks"/>
+    
+    <meta name="description" content="DjiWorks est le site personnel de Djothi Grondin 
+    vous permettant de d&eacute;couvrir l\'ensemble de ses cr&eacute;ations informatiques, 
+    multim&eacute;dias mais aussi photographiques."/>
     
     <meta name="author" content="Djothi Grondin">
-     <!--[if lt IE 9]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> 
-	<![endif]-->
 	
     <!-- Les styles -->
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
-    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
    	
    	<!-- Les scripts -->
-   	 
+   	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+     
 	
 	<!-- Favicon -->
 	<link rel="shortcut icon" href="./images/used/favicon.ico">
 	<link rel="apple-touch-icon" sizes="72x72" href="./images/used/touch-icon-ipad.png" />
 	<link rel="apple-touch-icon" sizes="114x114" href="./images/used/touch-icon-iphone-retina.png" />
 	<link rel="apple-touch-icon" sizes="144x144" href="./images/used/touch-icon-ipad-retina.png" />
-	
 </head>
 
-<body></body>
+<body>
+	<!-- Navigation Bar -->
+	<div class="navbar-wrapper">
+	<nav class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#"><img alt="Logo djiworks" src="images/used/logo.png"/></a>
+		    </div>
+		    
+		    
+		    <div class="collapse navbar-collapse">
+			    <ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Facebook</a></li>
+					<li><a href="#">Twitter</a></li>
+					<li><a href="#">Google+</a></li>
+					<li><a href="#">Viadeo</a></li>
+				</ul>
+		    </div>
+		</div>
+	</nav>
+	</div>
+	
+	<!-- Navigation Bar -->
+	<div id="slideshow" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#slideshow" data-slide-to="0" class="active"></li>
+        <li data-target="#slideshow" data-slide-to="1"></li>
+        <li data-target="#slideshow" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="images/slides/nuit.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="images/slides/nuit.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="images/slides/nuit.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#slideshow" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#slideshow" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
+		
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>	
+</body>
 </html>
