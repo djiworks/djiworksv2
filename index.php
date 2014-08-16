e06c5def091890c03b7929bb571f7761fff59e5e
Djothi Grondin <djothi.grondin@live.fr>
2014-08-16 10:20:52 +0400

    index

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Les meta -->
+    <title> DjiWorks, Site personnel de Djothi Grondin, Portefolio des photographies, du multim&eacute;dia et du d&eacute;veloppement web, mobile et logiciel</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="djothi, grondin, site personnel, djothi grondin, photo de djothi, site internet de djothi, site web de djothi, la r&eacute;union, &eacute;le de la r&eacute;union, 974, djiworks"/>
+    
+    <meta name="description" content="DjiWorks est le site personnel de Djothi Grondin vous permettant de d&eacute;couvrir l\'ensemble de ses cr&eacute;ations informatiques, multim&eacute;dias mais aussi photographiques."/>
+    
+    <meta name="author" content="Djothi Grondin">
+     <!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> 
+	<![endif]-->
+	
+    <!-- Les styles -->
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+    
+   	
+   	<!-- Les scripts -->
+   	 
+	
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="./images/used/favicon.ico">
+	<link rel="apple-touch-icon" sizes="72x72" href="./images/used/touch-icon-ipad.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="./images/used/touch-icon-iphone-retina.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="./images/used/touch-icon-ipad-retina.png" />
+	
+</head>
+
+<body></body>
+</html>
